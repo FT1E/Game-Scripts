805b41d7c04c65c872bfc93fb2519435d81ce036
FT1E <ft1hcfu@gmail.com>
2026-04-01 19:40:52 +0200

    Small commit - simple NavMesh setup on enemies

--- Entities/Enemy.cs
+++ Entities/Enemy.cs
@@ -3,6 +3,14 @@ using UnityEngine.AI;
 
 public class Enemy : Entity
 {
-    
-    
+    private NavMeshAgent _nmAgent;
+    public NavMeshAgent nmAgent { get{return _nmAgent;} }
+
+    public bool manualMove;
+
+    void Awake()
+    {
+        _nmAgent = GetComponent<NavMeshAgent>();
+    }
+
 }
--- Entities/EnemyManager.cs
+++ Entities/EnemyManager.cs
@@ -5,25 +5,57 @@ using UnityEngine.AI;
 public class EnemyManager : MonoBehaviour {
     
     // todo - object pooling
+    private Stack<Enemy> inactiveEnemies;
 
     [SerializeField]
     private PlayerInfo playerInfo = default;
     // above is for setting target position of NavMeshAgents
 
+
+    // list of active enemies
+    [SerializeField]
+    private List<Enemy> activeEnemies;
+
+
     void Awake()
     {
-        
+        foreach(Transform child in transform)
+        {
+            activeEnemies.Add(child.GetComponent<Enemy>());
+        }
     }
 
+    // todo - spawning
+    // todo - despawning/killing enemies
     public void SpawnEnemy(int count)
     {
         throw new System.NotImplementedException();
     }
 
-    
-
-    void Update()
+    void Start()
     {
         
     }
+
+    void Update()
+    {
+        foreach(Enemy enemy in activeEnemies)
+        {
+            if(Vector3.Distance(enemy.transform.position, playerInfo.position) < 2f)
+            {
+                // disable agent
+                enemy.nmAgent.isStopped = true;
+                // start attack
+
+            }
+            else
+            {
+                // enable agent
+                enemy.nmAgent.isStopped = false;
+                // follow player
+                enemy.nmAgent.destination = playerInfo.position;
+            }
+        }
+    }
+
 }
